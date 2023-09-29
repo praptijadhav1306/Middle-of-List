af8a91c5bae47c400ba91a724ec9e891b799dc20
praptijadhav1306 <140963643+praptijadhav1306@users.noreply.github.com>
2023-09-29 20:40:08 +0530

    Add files via upload

---
+++ CountMethod.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node{
+    int data;
+    node *next;
+};
+void printlist(node*head){
+    node * temp = head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+node * middleElement(node *head){
+    int count =0;
+    node * temp =head;
+
+    while(temp!=NULL){
+        count++;
+        temp = temp->next;
+    }
+
+    count/=2;
+
+    int i=0;
+    node *ans;
+
+    while(i<=count){
+        ans = head;
+        head = head->next;
+        i++;
+    }
+    return ans;
+}
+int main(){
+
+    node *head = new node();
+    node *first = new node();
+    node *second = new node();
+    node *third = new node();
+    node *fourth = new node();
+
+    head->data = 1;
+    head->next = first;
+    first->data = 2;
+    first->next = second;
+    second->data = 3;
+    second->next = third;
+    third->data = 4;
+    third->next = fourth;
+    fourth->data = 5;
+    fourth->next = NULL;
+
+    cout<<second<<endl;
+
+    printlist(head);
+    node *ans=middleElement(head);
+    cout<<"The middle element address is: "<<ans<<endl;
+
+    if(second == ans){
+        cout<<"Middle elememt maches";
+    }
+    else{
+        cout<<"Invalid middle element";
+    }
+    return 0;
+}
